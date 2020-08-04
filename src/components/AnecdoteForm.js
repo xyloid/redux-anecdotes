e8f3c69c139f8e18994d440428b1a3c52975b27b
yilin xu <xu.yilin@husky.neu.edu>
2020-08-03 20:08:19 -0400

    update ex6.13/14

--- src/components/AnecdoteForm.js
+++ src/components/AnecdoteForm.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = (props) => {
   const dispatch = useDispatch();
-  const addAnecdote = (event) => {
+  const addAnecdote = async (event) => {
     event.preventDefault();
     const content = event.target.content.value;
-    dispatch(createAnecdote(content));
     event.target.content.value = "";
+    const res = await anecdoteService.createNew(content)
+    dispatch(createAnecdote(res));
+    
   };
 
   return (
--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -1,26 +1,26 @@
-const anecdotesAtStart = [
-  "If it hurts, do it more often",
-  "Adding manpower to a late software project makes it later!",
-  "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
-  "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
-  "Premature optimization is the root of all evil.",
-  "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
-];
+// const anecdotesAtStart = [
+//   "If it hurts, do it more often",
+//   "Adding manpower to a late software project makes it later!",
+//   "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+//   "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+//   "Premature optimization is the root of all evil.",
+//   "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+// ];
 
-const getId = () => (100000 * Math.random()).toFixed(0);
+// const getId = () => (100000 * Math.random()).toFixed(0);
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0,
-  };
-};
+// const asObject = (anecdote) => {
+//   return {
+//     content: anecdote,
+//     // id: getId(),
+//     votes: 0,
+//   };
+// };
 
-export const createAnecdote = (content) => {
+export const createAnecdote = (data) => {
   return {
     type: "NEW_ANECDOTE",
-    data: { ...asObject(content) },
+    data,
   };
 };
 
@@ -31,9 +31,16 @@ export const voteAnecdote = (id) => {
   };
 };
 
-const initialState = anecdotesAtStart.map(asObject);
+export const initAnecdotes = (anecdotes) => {
+  return {
+    type: "INIT_ANECDOTES",
+    data: anecdotes,
+  };
+};
 
-const reducer = (state = initialState, action) => {
+// const initialState = anecdotesAtStart.map(asObject);
+
+const reducer = (state = [], action) => {
   console.log("state now: ", state);
   console.log("action", action);
   switch (action.type) {
@@ -52,6 +59,8 @@ const reducer = (state = initialState, action) => {
         .map((a) => (a.id === id ? updated : a))
         .sort((a, b) => b.votes - a.votes);
     }
+    case "INIT_ANECDOTES":
+      return action.data;
     default:
       return state;
   }
